79dcd82bdc4054e1d809fb0b99ee5a815b86af57
Ahmed shehata <ahamdyshehata7@gmail.com>
2020-06-07 15:09:44 +0200

    edit backup feature

--- app/src/main/java/com/ashehata/cat_task1/ui/fragment/UsersFragment.java
+++ app/src/main/java/com/ashehata/cat_task1/ui/fragment/UsersFragment.java
@@ -1,19 +1,17 @@
 package com.ashehata.cat_task1.ui.fragment;
 
 import android.Manifest;
-import android.app.AlarmManager;
-import android.app.PendingIntent;
-import android.content.Context;
-import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.widget.Toolbar;
 import androidx.constraintlayout.widget.Group;
 import androidx.core.app.ActivityCompat;
@@ -22,14 +20,12 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.Observer;
 import androidx.navigation.NavController;
 import androidx.recyclerview.widget.RecyclerView;
-import androidx.sqlite.db.SimpleSQLiteQuery;
 
 import com.ashehata.cat_task1.R;
 import com.ashehata.cat_task1.adapter.UsersAdapter;
 import com.ashehata.cat_task1.data.local.room.AppDatabase;
 import com.ashehata.cat_task1.data.local.room.User;
 import com.ashehata.cat_task1.ui.activity.HomeActivity;
-import com.ashehata.cat_task1.utility.BackupUtilKt;
 import com.ashehata.cat_task1.utility.HelperMethod;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
@@ -37,9 +33,11 @@ import com.google.android.material.snackbar.Snackbar;
 import org.apache.commons.io.FileUtils;
 
 import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 import java.util.concurrent.Executors;
-import java.util.logging.FileHandler;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -53,6 +51,8 @@ public class UsersFragment extends Fragment {
     RecyclerView usersFragmentRvUsers;
     @BindView(R.id.users_fragment_group_no_user)
     Group usersFragmentGroupNoUser;
+    @BindView(R.id.progress)
+    ProgressBar progress;
 
     private NavController navController;
     private LiveData<List<User>> usersData;
@@ -91,16 +91,16 @@ public class UsersFragment extends Fragment {
         // set items click listener
         mToolbar.setOnMenuItemClickListener(item -> {
             switch (item.getItemId()) {
-                case R.id.action_delete_all :
+                case R.id.action_delete_all:
                     deleteAllUsers();
                     break;
-                case R.id.action_dummy_user :
+                case R.id.action_dummy_user:
                     insertDummyUser();
                     break;
-                case R.id.create_backup :
+                case R.id.create_backup:
                     createBackup();
                     break;
-                case R.id.upload_backup :
+                case R.id.upload_backup:
                     uploadBackup();
                     break;
             }
@@ -109,81 +109,111 @@ public class UsersFragment extends Fragment {
     }
 
     private void createBackup() {
-        // first close database file to make any action safely
-        //AppDatabase.getInstance(getContext()).close();
-        /*
-        BackupUtilKt.createBackup(getContext(), dataBaseName);
-        Toast.makeText(getContext(), "Done", Toast.LENGTH_SHORT).show();
 
-         */
         int permission = ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE);
 
-        if(permission == PackageManager.PERMISSION_GRANTED ) {
-            AppDatabase database = AppDatabase.getInstance(getContext());
-            //database.userDao().checkpoint(new SimpleSQLiteQuery("pragma wal_checkpoint(full)"));
-            //database.close();
+        if (permission == PackageManager.PERMISSION_GRANTED) {
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                progress.setProgress(70, true);
+
+            } else {
+                progress.setProgress(70);
+
+            }
 
             File db = getContext().getDatabasePath(dataBaseName);
-            File dbShm = new File(db.getParent(), dataBaseName+"-shm");
-            File dbWal = new File(db.getParent(), dataBaseName+"-wal");
+            File dbShm = new File(db.getParent(), dataBaseName + "-shm");
+            File dbWal = new File(db.getParent(), dataBaseName + "-wal");
 
             File db2 = new File(getContext().getCacheDir(), dataBaseName);
-            File dbShm2 = new File(db2.getParent(), dataBaseName+"-shm");
-            File dbWal2 = new File(db2.getParent(), dataBaseName+"-wal");
+            File dbShm2 = new File(db2.getParent(), dataBaseName + "-shm");
+            File dbWal2 = new File(db2.getParent(), dataBaseName + "-wal");
 
             try {
                 FileUtils.copyFile(db, db2);
-//                Toast.makeText(getContext(), database.isOpen()+"", Toast.LENGTH_SHORT).show();
                 FileUtils.copyFile(dbShm, dbShm2);
                 FileUtils.copyFile(dbWal, dbWal2);
+                Toast.makeText(getContext(), "Backup successfully :)", Toast.LENGTH_SHORT).show();
+
             } catch (Exception e) {
                 Log.e("SAVEDB", e.toString());
+                Toast.makeText(getContext(), "Backup faild :(", Toast.LENGTH_SHORT).show();
+
+            } finally {
+                progress.setVisibility(View.GONE);
+
             }
 
-
-            } else {
-                Snackbar.make(getView(), "Please allow access to your storage", Snackbar.LENGTH_LONG)
-                        .setAction("Allow", view -> ActivityCompat.requestPermissions(getActivity(), new String[] {
-                                Manifest.permission.WRITE_EXTERNAL_STORAGE
-                        }, 0)).show();
-            }
+        } else {
+            Snackbar.make(getView(), "Please allow access to your storage", Snackbar.LENGTH_LONG)
+                    .setAction("Allow", view -> ActivityCompat.requestPermissions(getActivity(), new String[]{
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE
+                    }, 0)).show();
+        }
 
     }
 
     private void uploadBackup() {
-//        BackupUtilKt.restoreBackup(getContext(), dataBaseName);
+
         int permission = ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE);
-
-        if(permission == PackageManager.PERMISSION_GRANTED ) {
-//            AppDatabase.getInstance(getContext()).close();
-
-            File db = new File(getContext().getCacheDir(), dataBaseName);
-            File dbShm = new File(db.getParent(), dataBaseName+"-shm");
-            File dbWal = new File(db.getParent(), dataBaseName+"-wal");
-
-            File db2 = getContext().getDatabasePath(dataBaseName);
-            File dbShm2 = new File(db2.getParent(), dataBaseName+"-shm");
-            File dbWal2 = new File(db2.getParent(), dataBaseName+"-wal");
-
-            try {
-                FileUtils.copyFile(db, db2);
-                //db2.delete();
-                FileUtils.copyFile(dbShm, dbShm2);
-                FileUtils.copyFile(dbWal, dbWal2);
-                reopenApp();
-            } catch (Exception e) {
-                Log.e("RESTOREDB", e.toString());
-            }
+        if (permission == PackageManager.PERMISSION_GRANTED) {
+            restoreBackup();
         } else {
+
             Snackbar.make(getView(), "Please allow access to your storage", Snackbar.LENGTH_LONG)
-                    .setAction("Allow", view -> ActivityCompat.requestPermissions(getActivity(), new String[] {
+                    .setAction("Allow", view -> ActivityCompat.requestPermissions(getActivity(), new String[]{
                             Manifest.permission.READ_EXTERNAL_STORAGE,
                             Manifest.permission.WRITE_EXTERNAL_STORAGE
                     }, 0)).show();
         }
     }
 
-    private void reopenApp() {
+    private void restoreBackup() {
+        File db = new File(getContext().getCacheDir(), dataBaseName);
+        File dbshm = new File(db.getParent(), dataBaseName + "-shm");
+        Date date = new Date(dbshm.lastModified());
+        SimpleDateFormat format = new SimpleDateFormat("EEE, MMM d, hh:mm aaa", Locale.ENGLISH);
+        String dateFormat = format.format(date);
+
+        AlertDialog alertDialog = new AlertDialog.Builder(getContext())
+                .setTitle("Restore a backup")
+                .setCancelable(false)
+                .setMessage("When you restore a backup, you 'll lose the current data.\n" +
+                        "Last backup date: " + dateFormat)
+                .setPositiveButton("Ok", (dialog, which) -> {
+                    // get backup file
+                    replaceCurrent(db, dbshm);
+                })
+                .setNegativeButton("Cancel", (dialog, which) -> {
+                    dialog.dismiss();
+                }).create();
+
+        alertDialog.show();
+
+    }
+
+    private void replaceCurrent(File currentBackup, File shmFile) {
+
+        File db = currentBackup;
+        File dbShm = shmFile;
+        File dbWal = new File(db.getParent(), dataBaseName + "-wal");
+
+        File db2 = getContext().getDatabasePath(dataBaseName);
+        File dbShm2 = new File(db2.getParent(), dataBaseName + "-shm");
+        File dbWal2 = new File(db2.getParent(), dataBaseName + "-wal");
+
+        try {
+            FileUtils.copyFile(db, db2);
+            FileUtils.copyFile(dbShm, dbShm2);
+            FileUtils.copyFile(dbWal, dbWal2);
+            reopenActivity();
+        } catch (Exception e) {
+            Log.e("RESTOREDB", e.toString());
+        }
+    }
+
+    private void reopenActivity() {
 /*
         Intent mStartActivity = new Intent(getActivity(), HomeActivity.class);
         int mPendingIntentId = 123456;
@@ -193,13 +223,11 @@ public class UsersFragment extends Fragment {
         System.exit(0);
 */
 
-
         getActivity().finish();
         getActivity().overridePendingTransition(0, 0);
         startActivity(getActivity().getIntent());
         getActivity().overridePendingTransition(0, 0);
 
-        /**/
     }
 
     private void insertDummyUser() {
@@ -229,9 +257,9 @@ public class UsersFragment extends Fragment {
         usersData.observe(getViewLifecycleOwner(), new Observer<List<User>>() {
             @Override
             public void onChanged(List<User> users) {
-                if(users.size() == 0){
+                if (users.size() == 0) {
                     usersFragmentGroupNoUser.setVisibility(View.VISIBLE);
-                }else {
+                } else {
                     usersFragmentGroupNoUser.setVisibility(View.GONE);
                 }
                 usersAdapter = new UsersAdapter(getContext(), users);
